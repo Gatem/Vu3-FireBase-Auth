65019077e885b76f931a708dca7367aecdb13df1
Sabry Elgatem <mr.sabryy@gmail.com>
2022-12-14 23:36:12 +0200

    login design

---
+++ src/modules/Auth/layout/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col-md-7 login-bg"></div>
+    <div class="col-md-5">
+      <router-view />
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.login-bg {
+  background-image: url("src/assets/login-bg-shadow.png");
+  background-size: 50%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: $grey-1;
+}
+</style>
